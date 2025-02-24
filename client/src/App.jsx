1b9dd8c8b79802b8c12be5931b6acaf7cc8395fe
Maeve-Norton <mnorton2@wpi.edu>
2025-02-24 15:15:27 -0500

    Server with Auth0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import Admin from './admin'; // Import the Admin page
-import Login from './Login'; // Import the Login page
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Admin from './admin'
+import Login from './Login'
 
 function App() {
-    const domain = 'dev-xf1gnvayvu76e2me.us.auth0.com';  // Replace with your Auth0 domain
-    const clientId = 'fGIjW4jWBEmttKpLr7HFiNbDRHCP3suZ';  // Replace with your Auth0 client ID
-
     return (
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            authorizationParams={{
-                redirect_uri: window.location.origin,
-            }}
-        >
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </Router>
-        </Auth0Provider>
-    );
+        <Router>
+            <Routes>
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+const domain = "dev-xf1gnvayvu76e2me.us.auth0.com";
+const clientId = "fGIjW4jWBEmttKpLr7HFiNbDRHCP3suZ";
+
+// Create a root and render your app
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Use createRoot
+root.render(
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        // This link needs to be changed to the glitch link
+        authorizationParams={{ redirect_uri: "http://localhost:5173" }}
+    >
         <App />
-    </React.StrictMode>
+    </Auth0Provider>
 );
+
