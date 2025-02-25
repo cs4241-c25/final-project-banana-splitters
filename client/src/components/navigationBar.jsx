cf4f6cdf8a4ee162b5379eb81133dda855b210ea
khulburt <khulburt121@gmail.com>
2025-02-25 14:53:31 -0500

    styling nav bar

--- client/src/components/navigationBar.jsx
+++ client/src/components/navigationBar.jsx
@@ -12,20 +12,22 @@ const NavigationBar = () => {
                 color: '#F5F5F5',
                 padding: '1rem',
                 boxShadow: '0 5px 4px rgba(0, 0, 0, 0.1)',
-                zIndex: 1000
+                zIndex: 1000,
+                display: 'flex',
+                flexDirection: 'column',
             }}>
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                     gap: '1rem',
+                    flex: 1,
                 }}>
                     <img
-                        src= {wpiCrest}
+                        src={wpiCrest}
                         alt="WPI Logo"
                         style={{width: '50px', height: '50px'}}
                     />
-
                     <h1 style={{
                         fontSize: '1.5rem',
                         fontWeight: 'bold',
@@ -35,19 +37,17 @@ const NavigationBar = () => {
                     </h1>
                 </div>
 
-                <nav>
-
+                <nav style={{marginTop: 'auto'}}>
                     <div style={{
                         display: 'flex',
-                        width: '100%', // Ensure it spans the whole screen
+                        width: '100%',
                         fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
                         justifyContent: 'space-evenly',
-                        marginTop: "25px"
                     }}>
-                        <a href="/" style={{color: '#F5F5F5'}}>Home</a>
-                        <a href="/selfcare" style={{color: '#F5F5F5'}}>Self Care</a>
+                        <a href="/" style={{color: '#F5F5F5', textDecoration: 'underline'}}>Home</a>
+                        <a href="/selfcare" style={{color: '#F5F5F5', textDecoration: 'underline'}}>Self Care</a>
                         <p style={{color: '#F5F5F5'}}>Care Report Form</p>
-                        <a href="/admin" style={{color: '#F5F5F5'}}>Admin</a>
+                        <a href="/admin" style={{color: '#F5F5F5', textDecoration: 'underline'}}>Admin</a>
                     </div>
                 </nav>
             </header>
